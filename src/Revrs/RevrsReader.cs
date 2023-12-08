bce5e8e46e43db7fcc78dcc1261f2c8673cfa1fa
ArchLeaders <archleadership28@gmail.com>
2023-12-08 02:20:23 -0800

    Create Revrs Reader/Writer (stub)

---
+++ src/Revrs/RevrsReader.cs
@@ -0,0 +1,9 @@
+﻿namespace Revrs;
+
+/// <summary>
+/// Casts <see langword="primitive"/> and <see langword="struct"/> data types over a <see cref="Span{T}"/> of bytes, reversing the underlying <see cref="Span{T}"/> when required.
+/// </summary>
+public ref struct RevrsReader
+{
+
+}
---
+++ src/Revrs/RevrsWriter.cs
@@ -0,0 +1,9 @@
+﻿namespace Revrs;
+
+/// <summary>
+/// Writes <see langword="primitive"/> and <see langword="struct"/> data types into a <see cref="Stream"/>, reversing the written values when required.
+/// </summary>
+public class RevrsWriter
+{
+
+}
