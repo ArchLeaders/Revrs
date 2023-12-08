00e164521275016b44218e20e1e7be7119f00f8c
ArchLeaders <archleadership28@gmail.com>
2023-12-08 02:05:15 -0800

    Define struct reverser interface

---
+++ src/Revrs/IStructReverser.cs
@@ -0,0 +1,16 @@
+﻿namespace Revrs;
+
+/// <summary>
+/// Defines a static function to reverse a <see cref="Span{T}"/> in predefined slices.
+/// <para>
+/// For optimal performance, <see cref="IStructReverser"/> should be implemented by a dedicated <see langword="class"/> inside the target <see langword="struct"/>, not implemented by the <see langword="struct"/> itself.
+/// </para>
+/// </summary>
+public interface IStructReverser
+{
+    /// <summary>
+    /// Reverses the <paramref name="buffer"/> in predefined slices to match the target <see langword="struct"/>.
+    /// </summary>
+    /// <param name="buffer"></param>
+    public abstract void Reverse(in Span<byte> buffer);
+}
