9f12afb4581cbe9107b87f94bf2081911583ccf5
ArchLeaders <archleadership28@gmail.com>
2024-02-24 15:04:12 -0800

    Add ReadOnlySpan read extensions

--- src/Revrs/Extensions/ReaderExtensions.cs
+++ src/Revrs/Extensions/ReaderExtensions.cs
@@ -40,6 +40,18 @@ public static class ReaderExtensions
     }
 
     /// <summary>
+    /// Read <typeparamref name="T"/> from the provided <paramref name="slice"/> in the system endianness.
+    /// </summary>
+    /// <typeparam name="T">The primitive or struct type to read.</typeparam>
+    /// <param name="slice">The data to read from.</param>
+    /// <returns>A reference to <typeparamref name="T"/> over a span of data.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static T Read<T>(this ReadOnlySpan<byte> slice) where T : unmanaged
+    {
+        return MemoryMarshal.Read<T>(slice);
+    }
+
+    /// <summary>
     /// Read <typeparamref name="T"/> from the provided <paramref name="stream"/> in the provided <paramref name="endianness"/>.
     /// <para>
     /// <b>Warning: </b> Only read <a href="https://learn.microsoft.com/en-us/dotnet/api/system.type.isprimitive">primitive types</a>
@@ -149,6 +161,18 @@ public static class ReaderExtensions
     /// <returns>A new <see cref="Span{T}"/> where the length is the <paramref name="slice"/> length / <see langword="sizeof"/>(<typeparamref name="T"/>).</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Span<T> ReadSpan<T>(this Span<byte> slice) where T : unmanaged
+    {
+        return MemoryMarshal.Cast<byte, T>(slice);
+    }
+
+    /// <summary>
+    /// Read <typeparamref name="T"/>'s from the provided <paramref name="slice"/> in the system <see cref="Endianness"/> until the end of the <paramref name="slice"/>.
+    /// </summary>
+    /// <typeparam name="T">The <see langword="unmanaged"/> <see langword="primitive"/> or <see langword="struct"/> type to read</typeparam>
+    /// <param name="slice">The data to read from.</param>
+    /// <returns>A new <see cref="Span{T}"/> where the length is the <paramref name="slice"/> length / <see langword="sizeof"/>(<typeparamref name="T"/>).</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ReadOnlySpan<T> ReadSpan<T>(this ReadOnlySpan<byte> slice) where T : unmanaged
     {
         return MemoryMarshal.Cast<byte, T>(slice);
     }
